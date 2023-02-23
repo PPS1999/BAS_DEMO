e3ab5c7b212d4c4b1d9b5b2b3409700339303c38
Priyanshu Pratap Singh <prpratapsingh@deloitte.com>
2023-02-23 08:26:40 +0000

    My first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["demobas/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"demo_bas/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
